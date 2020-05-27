5e383983cabb77c47faa82aa096038492a16a822
Aziz Rafsanjani <coded_raf@yahoo.com>
2020-05-27 16:36:05 +0100

    update website

--- sendmail.php
+++ sendmail.php
@@ -1,2 +1,15 @@
 <?php
-echo 'Sending mail';
+     if (isset($_POST['name'])) {
+        $name = $_REQUEST['name'];
+        $email = $_REQUEST['email'];
+        $message = $_REQUEST['message'];
+    
+       $to = 'coded_raf@yahoo.com';
+       $subject = 'Mail from foreverrafs.com';
+       $headers = "From: ".$name." <".$email."> \r\n";
+       $send_email = mail($to,$subject,$message,$headers);
+        
+
+       echo ($send_email) ? "sent" : "failed";
+  }
+?>
